ae21ab07e6ef9d45bd531bc532265edd255731f2
zley <zhu14leyi1412@gmail.com>
2026-05-09 15:12:17 +0800

    Remove dynamic ads route and disabled middleware

--- app/ads.txt/route.ts
+++
@@ -1,9 +0,0 @@
-export function GET() {
-  const body = 'google.com, pub-0229439021577089, DIRECT, f08c47fec0942fa0\n';
-
-  return new Response(body, {
-    headers: {
-      'content-type': 'text/plain; charset=utf-8'
-    }
-  });
-}
